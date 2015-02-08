9b6b5fafcc38689b3777f79cc71fcafe4615883e
Carlos Chiconato <cachiconato@gmail.com>
2015-02-07 22:58:53 -0200

    removing unused code

--- diarias_app.rb
+++ diarias_app.rb
@@ -5,8 +5,6 @@ require 'json'
 class DiariasApp < Sinatra::Base
   helpers Sinatra::JSON
 
-  @@agencies ||= JSON.parse File.read('data/data.json')
-
   configure do
     set :views, Proc.new { File.join(root, "public/views") }
   end
