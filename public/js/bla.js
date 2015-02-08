db23121edf8da6ddca7ac1328a26fc4e5b9d1976
Carlos Chiconato <cachiconato@gmail.com>
2015-02-08 01:16:37 -0200

    adding some colors to the graph

--- public/js/bla.js
+++ public/js/bla.js
@@ -1,6 +1,6 @@
 var tableid = '1P9RV33IwVqQ2pNInbjVLGfqbQKb4mQhp2TQ_Bxet'
 //var query = 'SELECT NOMEPESSOA,EXERCICIO,NOME_ORGAO,NOME_PROJATIV,EMPENHO,EMPENHADO,LIQUIDADO FROM ' + tableid
-var query = 'SELECT NOME_PROJATIV,NOME_ORGAO,LIQUIDADO FROM ' + tableid
+var query = 'SELECT ID_ZPRT_DESP_COM_DIARIAS,NOME_ORGAO,LIQUIDADO FROM ' + tableid
 var key = 'AIzaSyCmJqyDLGq5UEcn0hpFO4hVhb5q74gVyLw'
 var url = 'https://www.googleapis.com/fusiontables/v2/query?sql=' + query + '&key=' + key
 
@@ -8,15 +8,22 @@ google.load("visualization", "1", {packages:["treemap"]})
 google.setOnLoadCallback(fetchDataFromFusionTables);
 
 function drawChart(res) {
-	
-	var bla = [ res.columns ].concat(res.rows);
-	console.log(res)
-	console.log(bla)
-	var data = google.visualization.arrayToDataTable(bla);
-	
+
+  console.log(res.rows);
+  var chartData = new google.visualization.DataTable();
+  chartData.addColumn('string', 'lixo');
+  chartData.addColumn('string', 'Parent');
+  chartData.addColumn('number', 'Valor');
+  chartData.addColumn('number', 'increase/decrease (color)');
+
+  chartData.addRow(['LIXAO', null, 0, 0]);
+
+  var count = 10;
+  _.each(res.rows, function(row) { count+=100; chartData.addRow([row[0], 'LIXAO', row[2], count]) });
+
 	var tree = new google.visualization.TreeMap(document.getElementById('treeGraph'));
 
-	tree.draw(data, {
+	tree.draw(chartData, {
 	  minColor: '#f00',
 	  midColor: '#ddd',
 	  maxColor: '#0d0',
