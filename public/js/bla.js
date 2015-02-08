7c1c4f0e0975b86ceeacaa7470b616970b1b72b1
Rodrigo Duarte <duarterodrigod@gmail.com>
2015-02-08 01:33:42 -0200

    fetching parent/child orgao/proj

--- public/js/bla.js
+++ public/js/bla.js
@@ -1,29 +1,21 @@
 var tableid = '1P9RV33IwVqQ2pNInbjVLGfqbQKb4mQhp2TQ_Bxet'
 //var query = 'SELECT NOMEPESSOA,EXERCICIO,NOME_ORGAO,NOME_PROJATIV,EMPENHO,EMPENHADO,LIQUIDADO FROM ' + tableid
-var query = 'SELECT ID_ZPRT_DESP_COM_DIARIAS,NOME_ORGAO,LIQUIDADO FROM ' + tableid
+var query = 'SELECT ID_ZPRT_DESP_COM_DIARIAS,ORGAO,LIQUIDADO FROM ' + tableid
+var queryPais = 'SELECT ORGAO FROM ' + tableid
 var key = 'AIzaSyCmJqyDLGq5UEcn0hpFO4hVhb5q74gVyLw'
 var url = 'https://www.googleapis.com/fusiontables/v2/query?sql=' + query + '&key=' + key
 
 google.load("visualization", "1", {packages:["treemap"]})
 google.setOnLoadCallback(fetchDataFromFusionTables);
 
-function drawChart(res) {
-
-  console.log(res.rows);
-  var chartData = new google.visualization.DataTable();
-  chartData.addColumn('string', 'lixo');
-  chartData.addColumn('string', 'Parent');
-  chartData.addColumn('number', 'Valor');
-  chartData.addColumn('number', 'increase/decrease (color)');
-
-  chartData.addRow(['LIXAO', null, 0, 0]);
-
-  var count = 10;
-  _.each(res.rows, function(row) { count+=100; chartData.addRow([row[0], 'LIXAO', row[2], count]) });
-
+function drawChart(bla) {
+	
+	console.log(bla)
+	var data = google.visualization.arrayToDataTable(bla);
+	
 	var tree = new google.visualization.TreeMap(document.getElementById('treeGraph'));
 
-	tree.draw(chartData, {
+	tree.draw(data, {
 	  minColor: '#f00',
 	  midColor: '#ddd',
 	  maxColor: '#0d0',
@@ -35,6 +27,20 @@ function drawChart(res) {
 }
 
 function fetchDataFromFusionTables() {
-	$.get(url, drawChart )
+	$.get('https://www.googleapis.com/fusiontables/v2/query?sql=' + queryPais + '&key=' + key, function(pais) {
+		console.log(pais)
+		$.get(url, function(filhos) {
+			console.log(filhos)
+			var table = [ filhos.columns ].concat([['LIXO', null, 0]]).concat(filhos.rows)
+			var lixo = []
+			_.each(pais.rows, function(el) {
+				if (lixo.indexOf(el[0]) == -1) { 
+					lixo = lixo.concat(el[0]);
+					table = table.concat([[el[0],'LIXO',0]]) 
+				}
+			})
+			drawChart(table);
+		} )	
+	} )
 
 }
